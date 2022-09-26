4f2423a769e97e458a2861166269b51995cfac41
Wouter Bosch <wfcbosch@gmail.com>
2022-09-26 11:05:39 +0100

    more precise approach

--- framerate_simple.py
+++ framerate_simple.py
@@ -1,4 +1,4 @@
-import pygame, sys
+import pygame, sys, time
 from debug import debug
 
 pygame.init()
@@ -6,10 +6,14 @@ screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 
 test_rect = pygame.Rect(0, 310, 100, 100)
+test_rect_pos = test_rect.x
 test_speed = 200
 
+previous_time = time.time()
 while True:
-    dt = clock.tick(10) / 1000
+    # dt = clock.tick(600) / 1000
+    dt = time.time() - previous_time
+    previous_time = time.time()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
@@ -18,8 +22,10 @@ while True:
     screen.fill('white')
 
     # Rect movement and drawing
-    test_rect.x += test_speed * dt # deltatime
+    test_rect_pos += test_speed * dt # deltatime
+    test_rect.x = round(test_rect_pos)
     pygame.draw.rect(screen, 'red', test_rect)
 
+    debug(test_speed * dt)
     pygame.display.update()
     
