b4287cd542e3f4977643df15884b935a28eb89d9
Wouter Bosch <wfcbosch@gmail.com>
2022-09-26 11:29:34 +0100

    more complex example

--- framerate_simple.py
+++ framerate_simple.py
@@ -14,6 +14,7 @@ while True:
     # dt = clock.tick(600) / 1000
     dt = time.time() - previous_time
     previous_time = time.time()
+    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
